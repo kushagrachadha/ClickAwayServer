53c398e2e58541f3467adb5654f9bd6e7835ab0e
chadha <chatchadha@gmail.com>
2017-02-10 21:43:30 +0530

    hackdtu

--- index.js
+++ index.js
@@ -20,11 +20,9 @@ app.get('/', function (req, res)
             res.send('Main View for the hospital view.');
         });
 app.get('/h1/',function(req,res){
-        var harr=[];
-
-        harr.push(req.query.h);
-        var a=req.query.h.length;
-        res.send(a.string);
+        var harr=req.query.h;
+        var len=harr.length;
+        res.send("hospitals are"+harr+" and length is"+len.toString());
 });
 
 
